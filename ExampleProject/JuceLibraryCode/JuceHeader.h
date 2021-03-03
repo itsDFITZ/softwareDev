9e3c80a133dfc344c2020f2a47f2ed0610096cf9
Dylan Page <dfitzpage@MacBook-Pro-2.attlocal.net>
2021-03-03 12:28:21 -0600

    early steps

--- ExampleProject/JuceLibraryCode/JuceHeader.h
+++ ExampleProject/JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "ExampleProject";
-    const char* const  companyName    = "";
+    const char* const  companyName    = "DFITZ SP";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
--- ExampleProject/Source/PluginEditor.h
+++ ExampleProject/Source/PluginEditor.h
@@ -14,7 +14,9 @@
 //==============================================================================
 /**
 */
-class ExampleProjectAudioProcessorEditor  : public juce::AudioProcessorEditor
+class ExampleProjectAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                            public juce::Slider::Listener,
+                                            public juce::Button::Listener
 {
 public:
     ExampleProjectAudioProcessorEditor (ExampleProjectAudioProcessor&);
@@ -23,11 +25,17 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void sliderValueChanged(juce::Slider * slider) override;
+    void buttonClicked(juce::Button * button) override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     ExampleProjectAudioProcessor& audioProcessor;
+    
+    juce::Slider gainSlider;
+    
+    juce::ToggleButton muteButton;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ExampleProjectAudioProcessorEditor)
 };
